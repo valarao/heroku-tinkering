5c1d40bcedf161ca23760db05ee2eb5b6ade86a8
unknown <miguel.valarao@gmail.com>
2021-05-27 15:32:28 -0700

    Implement frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,83 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
+import axios from 'axios';
+
+
 
 function App() {
+  const [users, setUsers] = useState(null);
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+
+  useEffect(() => {
+    const getUsers = async () => {
+      try {
+        const users = await axios.get("/api/users");
+        setUsers(users.data);
+        console.log(users);
+      } catch (err) {
+        console.log(err);
+      }
+    }
+    getUsers();
+  }, []);
+
+  const submitForm = async () => {
+    if (username === "") {
+      alert("Please fill the username field");
+      return;
+    }
+    if (email === "") {
+      alert("Please fill the email field");
+      return;
+    }
+    try {
+      await axios.post("/api/users", {
+        name: username,
+        email: email,
+      });
+     
+      alert("Account created successfully");
+      window.location.reload();
+    } catch (err) {
+      alert("Could not create account. Please try again");
+    }
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>My Project</h1>
+      {users === null ? (
+        <p>Loading...</p>
+      ) : users.length === 0 ? (
+        <p>No user available</p>
+      ) : (
+        <>
+          <h2>Available Users</h2>
+          <ol>
+            {users.map((user, index) => (
+              <li key={index}>
+                Name: {user.name} - Email: {user.email}
+              </li>
+            ))}
+          </ol>
+        </>
+      )}
+
+      <form>
+        <input
+          onChange={(e) => setUsername(e.target.value)}
+          type="text"
+          placeholder="Enter your username"
+        />
+        <input
+          onChange={(e) => setEmail(e.target.value)}
+          type="text"
+          placeholder="Enter your email address"
+        />
+        <input type="button" value='Submit' onClick={submitForm} />
+      </form>
+    </>
   );
 }
 
